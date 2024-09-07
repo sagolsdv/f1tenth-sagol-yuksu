e76252dfcdcc0991903869c7fbe9f69f8ead2ee6
JoonCheol Park <jooncheol@gmail.com>
2024-09-07 18:27:51 +0400

    fix model path in run-sagol-rl

--- scripts/run-sagol-rl.sh
+++ scripts/run-sagol-rl.sh
@@ -4,4 +4,4 @@ source /opt/ros/galactic/setup.bash
 source /sagol_ws/install/setup.bash
 
 cd /sagol_ws/src/sagol
-python3 sagol/rl_tf_driver.py --model savedmodel/model
+python3 sagol/rl_tf_driver.py --model savedmodel/models/
